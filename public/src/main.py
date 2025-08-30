ff02aeca9192ae97726480501635e32c2147b03e
kitanaya <seb.heinzmann@gmail.com>
2025-08-31 01:21:25 +0200

    restructuring

--- public/src/main.py
+++
@@ -1,8 +0,0 @@
-from textnode import TextNode
-
-def main():
-    textnode1 = TextNode("some text", "link", "https://boot.dev")
-    print(repr(textnode1))
-
-main()
-#left hand kinda OK again..really kinda...
